424c27f6cc16ea754fb884a099f8307fa154c897
William John Holden <wjholden@gmail.com>
2019-12-01 07:03:00 +0100

    Update aoc-2019-1.jl

--- 1/aoc-2019-1.jl
+++ 1/aoc-2019-1.jl
@@ -1,12 +1,14 @@
 using DelimitedFiles
 
+input = readdlm("input.txt",' ',Int,'\n')
+
 # Part 1
 fuel_required(mass) = (mass ÷ 3) - 2
-part1 = reduce(+, map(fuel_required, readdlm("input.txt",' ',Int,'\n')))
+part1 = reduce(+, map(fuel_required, input))
+println("Part 1: ", part1)
 
 # Part 2
 fuel_required_2(mass) = (fuel_required(mass) > 0) ? fuel_required(mass) + fuel_required_2(fuel_required(mass)) : 0
-part2 = reduce(+, map(fuel_required_2, readdlm("input.txt",' ',Int,'\n')))
+part2 = reduce(+, map(fuel_required_2, input))
 
-println("Part 1: ", part1)
 println("Part 2: ", part2)
