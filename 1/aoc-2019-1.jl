4ef09f2c94012918a9a4ccc0f0f26eb2c278d052
William John Holden <wjholden@gmail.com>
2019-12-01 07:22:22 +0100

    Update aoc-2019-1.jl

--- 1/aoc-2019-1.jl
+++ 1/aoc-2019-1.jl
@@ -1,15 +1,18 @@
 using DelimitedFiles
 
-input = readdlm("input.txt",' ',Int,'\n')
+input = readdlm("input.txt",' ',Int,'\n');
 
 # Part 1
-fuel_required(mass) = (mass ÷ 3) - 2
+fuel_required(mass) = (mass ÷ 3) - 2;
 part1 = reduce(+, map(fuel_required, input))
 println("Part 1: ", part1)
 
 # Part 2
-fuel_required_2(mass) = (fuel_required(mass) > 0) ? fuel_required(mass) + fuel_required_2(fuel_required(mass)) : 0
+fuel_required_2(mass) = (fuel_required(mass) > 0) ? fuel_required(mass) + fuel_required_2(fuel_required(mass)) : 0;
 part2 = reduce(+, map(fuel_required_2, input))
 println("Part 2: ", part2)
 
 # Nothing too crazy here for those familiar with recursion, although reading the instructions carefully always helps.
+# Notice something interesting: the part 2 function does not distribute. That is, you cannot compute
+#   fuel_required_2(part1) + part1
+# to get part2. 
