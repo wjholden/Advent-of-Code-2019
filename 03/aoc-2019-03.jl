2d49245167aa051e57270568c404b096c695e979
William John Holden <wjholden@gmail.com>
2019-12-03 19:08:22 +0100

    Update aoc-2019-03.jl
    Use complex arithmetic instead of (x,y) variables. Replace switch statement with a more compact/generalizable dictionary. Simplify Manhattan function based on safe assumptions from specification.

--- 03/aoc-2019-03.jl
+++ 03/aoc-2019-03.jl
@@ -1,7 +1,6 @@
 using DelimitedFiles
 
-#input = readdlm("example3.txt", ',', String, '\n');
-input = readdlm("input.txt", ',', String, '\n');
+input = readdlm(ARGS[1], ',', String, '\n');
 function parseDirection(element::String)
     return (h=element[1],
         δ=parse(Int, element[2:end]))
@@ -17,27 +16,14 @@ wire1 = parseDirection.(wire1);
 wire2 = parseDirection.(wire2);
 
 function explore(turns)
-    (x,y) = (0,0)
+    position = 0
     path = Dict()
+    inc = Dict('U' => im, 'D' => -im, 'R' => 1, 'L' => -1)
     steps = 0
     for t in turns
-        d = 1
-        while d <= t.δ
-            if t.h == 'U'
-                y = y + 1
-            elseif t.h == 'D'
-                y = y - 1
-            elseif t.h == 'R'
-                x = x + 1
-            elseif t.h == 'L'
-                x = x - 1
-            else
-                println("Found $(t.h) value that is not U, D, L, or R.")
-                exit()
-            end
-            d = d + 1
+        for d in 1:t.δ
+            position = position + inc[t.h] # Not my idea but it is clever. Saves us from switching.
             steps = steps + 1
-            position = (x=x, y=y)
             if !(position in keys(path))
                 path[position] = steps
             end
@@ -50,11 +36,11 @@ path1 = explore(wire1)
 path2 = explore(wire2)
 crosses = collect(intersect(keys(path1), keys(path2)))
 
-function manhattan(p1, p2)
-    return abs(p2.x - p1.x) + abs(p2.y - p1.y)
+function manhattan(p)
+    return abs(real(p)) + abs(imag(p))
 end
 
-distances = map(p -> manhattan(p, (x=0,y=0)), crosses)
+distances = map(manhattan, crosses)
 println("Part 1: $(findmin(distances)[1])")
 
 steps = map(point -> path1[point] + path2[point], crosses)
