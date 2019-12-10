7fc2fef79255678c9b57144523f5b307014bde9e
William John Holden <wjholden@gmail.com>
2019-12-10 10:49:24 +0100

    Added test case

--- 09/aoc-2019-09.jl
+++ 09/aoc-2019-09.jl
@@ -4,7 +4,7 @@ using IntcodeVM;
 using DelimitedFiles;
 
 print("Day 9 Part 1: ");
-IntcodeVM.run(vec(readdlm("input.txt", ',', Int, '\n')), inputs=[1], out=stdout)
+IntcodeVM.run(vec(readdlm(ARGS[1], ',', Int, '\n')), inputs=[1], out=stdout)
 
 print("Day 9 Part 2: ");
-IntcodeVM.run(vec(readdlm("input.txt", ',', Int, '\n')), inputs=[2], out=stdout)
+IntcodeVM.run(vec(readdlm(ARGS[1], ',', Int, '\n')), inputs=[2], out=stdout)
