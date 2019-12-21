732ded50e899ae33d55dc678900e7d5486dbb5a7
William John Holden <wjholden@gmail.com>
2019-12-21 17:15:20 +0100

    Update aoc-2019-09.jl

--- 09/aoc-2019-09.jl
+++ 09/aoc-2019-09.jl
@@ -1,6 +1,12 @@
 using IntcodeVM;
 using DelimitedFiles;
 
+# In day 9 we complete the Intcode VM and validate its correctness.
+# Many Advent of Code participants (including myself) would have found
+# on this day that their Intcode interpreters were under-engineered.
+# The big thing people needed was encapsulation. Intcode VMs should be
+# easy to start, reasonably self-contained, and easy to get output from.
+
 print("Day 9 Part 1: ");
 IntcodeVM.run(vec(readdlm(ARGS[1], ',', Int, '\n')), inputs=[1], out=stdout)
 
